a9d8f14240a7434a7f38b8cbcecf50c50cd03b10
Arham Khawar <ch.arham1220@gmail.com>
2022-06-23 02:06:48 +0500

    bug fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 console.log("Connecting to MongoDB..", process.env.DB_URI);
 
 const connectToMongo = ()=>{
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.DB_URI || "mongodb+srv://apple:apple123@cluster0.gqruk.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then((data)=>console.log(`Connected to MongoDB with server: ${data.connection.host}:${process.env.PORT}`))
     .catch((err)=>console.log(`Connection Failed...err:${err}`));
      
